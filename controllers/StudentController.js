81db06818626928122f6b54337236c916f20bf9d
Josh Levine <levine.joshuaadam@gmail.com>
2022-11-22 11:16:21 -0500

    GPA calculation updates student record, and delivers new student record to front end, from GetStudent in StudentController

--- controllers/StudentController.js
+++ controllers/StudentController.js
@@ -11,14 +11,73 @@ const GetStudents = async (req, res) => {
 
 const GetStudent = async (req, res) => {
   try {
-    const student = await Students.findOne({
+    let student = await Students.findOne({
       where: {
         id: req.params.student_id
       },
       include: [Courses]
-      // include: [Courses, StudentsCourses]
     })
-    res.send(student)
+    let gpaPoints = 0
+    let totalCreditHours = 0
+
+    //map through courses
+    student.Courses.map((course) => {
+      let courseGpaPoints
+      totalCreditHours += course.creditHours
+
+      switch (course.StudentsCourses.letter) {
+        case 'A':
+          courseGpaPoints = 4 * course.creditHours
+          gpaPoints += courseGpaPoints
+          break
+        case 'B':
+          courseGpaPoints = 3 * course.creditHours
+          gpaPoints += courseGpaPoints
+          break
+        case 'C':
+          courseGpaPoints = 2 * course.creditHours
+          gpaPoints += courseGpaPoints
+          break
+        case 'D':
+          courseGpaPoints = 1 * course.creditHours
+          gpaPoints += courseGpaPoints
+          break
+        case 'F':
+          courseGpaPoints = 0 * course.creditHours // this line is unnecessary for the calculation because it is multiplying by 0
+          gpaPoints += courseGpaPoints
+          break
+        default:
+          return console.log('not a valid grade letter (A-F)')
+      }
+    })
+    let gpa = gpaPoints / totalCreditHours
+
+    // console.log(`GPA: ${gpa}`)
+    // update student record with new gpa, response will have new object... send that one
+    let studentId = parseInt(req.params.student_id)
+    const studentUpdate = await Students.update(
+      { gpa },
+      {
+        where: { id: studentId },
+        returning: true,
+        include: [Courses]
+      }
+    )
+    let updatedStudent = await Students.findOne({
+      where: {
+        id: req.params.student_id
+      },
+      include: [Courses]
+    })
+
+    // let body = { gpa }
+    // let request = { body }
+    // let studentUpdate = UpdateStudent(gpa, student.id)
+    // // student = { ...student, gpa }
+
+    // // console.log(student)
+    console.log(updatedStudent)
+    res.json(updatedStudent)
   } catch (error) {
     throw error
   }
@@ -33,15 +92,15 @@ const CreateStudent = async (req, res) => {
   }
 }
 
-const UpdateStudent = async (req, res) => {
+const UpdateStudent = async (gpa, studentId) => {
   try {
     const student = await Students.update(
-      { ...req.body },
-      { where: { id: req.params.student_id }, returning: true }
+      { gpa },
+      { where: studentId, returning: true }
     )
-    res.send(student)
+    return student
   } catch (error) {
-    return res.status(500).send(error.message)
+    return studentId.status(500).send(error.message)
   }
 }
 
